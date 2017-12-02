05a5c3a7221ae1ac172d3feca1e9d3c0537a016b
Michael Vasios <mvasios1@gmail.com>
2017-12-02 20:37:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-recipes = {}
+recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   clone = object;
   clone.key = value;
   clone;
-}
+};
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object.key = value;
   object
-}
+};
 
 function deleteFromObjectByKey(object, key) {
   clone = object;
   delete clone.key;
   clone
-}
+};
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key) {
   delete object.key;
   object
-}
+};
